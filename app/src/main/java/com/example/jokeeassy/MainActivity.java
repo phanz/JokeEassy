6d8b0afcea263072fc23e3a474ccc47ef3529036
phanz <phanz@qq.com>
2017-06-19 22:44:03 +0800

    搭建基本结构

--- app/src/main/java/com/example/jokeeassy/MainActivity.java
+++ app/src/main/java/com/example/jokeeassy/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.jokeeassy;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
+import android.widget.ImageView;
 
+import com.example.fragment.DiscoveryFragment;
+import com.example.fragment.HomeFragment;
+import com.example.fragment.MessageFragment;
+import com.example.fragment.ReviewFragment;
 import com.example.widget.TitleBar;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+    public static final String TAG = "MainActivity";
+
+    @BindView(R.id.home_image)
+    public ImageView mHomeImage;
+    @BindView(R.id.discovery_image)
+    public ImageView mDiscoveryImage;
+    @BindView(R.id.review_image)
+    public ImageView mReviewImage;
+    @BindView(R.id.msg_image)
+    public ImageView mMessageImage;
+
+    private Fragment mFragmentNow;
+
+    private Fragment mHomeFragment;
+    private Fragment mDiscoveryFragment;
+    private Fragment mReviewFragment;
+    private Fragment mMessageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,37 +44,68 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         if (Build.VERSION.SDK_INT >= 21) {
-            View decorView = getWindow().getDecorView();
-            /*int option = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            decorView.setSystemUiVisibility(option);*/
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().setStatusBarColor(getResources().getColor(R.color.colorAccent));
+            getWindow().setStatusBarColor(getResources().getColor(R.color.statusBar));
         }
         ButterKnife.bind(this);
-        TitleBar titleBar = (TitleBar) findViewById(R.id.title);
-        titleBar.setOnCustomClicklistener(new TitleBar.OnCommonClickListener() {
-
+        TitleBar titleBar = (TitleBar) findViewById(R.id.title_bar);
+        titleBar.setLeftImageResource(R.drawable.ic_discovery_default_avatar);
+        titleBar.setTitleBackground(R.drawable.ic_neihan_logo);
+        titleBar.setDividerColor(android.R.color.black);
+        titleBar.addAction(new TitleBar.ImageAction(R.drawable.ic_publish) {
             @Override
-            public void onCustomLeftClick(View view) {
-                Toast.makeText(getApplicationContext(), "left click", Toast.LENGTH_SHORT).show();
-            }
+            public void performAction(View view) {
 
-            @Override
-            public void onCustomRightClick(View view) {
-                Toast.makeText(getApplicationContext(), "right click", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCustomSearchClick(View view) {
-                Toast.makeText(getApplicationContext(), "title click", Toast.LENGTH_SHORT).show();
             }
         });
 
+        mHomeFragment = new HomeFragment();
+        mDiscoveryFragment = new DiscoveryFragment();
+        mReviewFragment = new ReviewFragment();
+        mMessageFragment = new MessageFragment();
+        changeFragment(mHomeFragment);
+
     }
 
-    @OnClick(R.id.click_btn)
-    public void onClick(View view) {
+    @OnClick({R.id.home_bottom_view, R.id.discovery_bottom_view,
+            R.id.review_bottom_view, R.id.msg_bottom_view})
+    public void onBottomClick(View view) {
+        mHomeImage.setBackgroundResource(R.drawable.ic_tab_home_normal);
+        mDiscoveryImage.setBackgroundResource(R.drawable.ic_tab_discovery_normal);
+        mReviewImage.setBackgroundResource(R.drawable.ic_tab_review_normal);
+        mMessageImage.setBackgroundResource(R.drawable.ic_tab_msg_normal);
 
-        Toast.makeText(this, "测试按钮", Toast.LENGTH_SHORT).show();
+        int id = view.getId();
+        switch (id){
+            case R.id.home_bottom_view:
+                mHomeImage.setBackgroundResource(R.drawable.ic_tab_home_pressed);
+                changeFragment(mHomeFragment);
+                break;
+            case R.id.discovery_bottom_view:
+                mDiscoveryImage.setBackgroundResource(R.drawable.ic_tab_discovery_pressed);
+                changeFragment(mDiscoveryFragment);
+                break;
+            case R.id.review_bottom_view:
+                mReviewImage.setBackgroundResource(R.drawable.ic_tab_review_pressed);
+                changeFragment(mReviewFragment);
+                break;
+            case R.id.msg_bottom_view:
+                mMessageImage.setBackgroundResource(R.drawable.ic_tab_msg_pressed);
+                changeFragment(mMessageFragment);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void changeFragment(Fragment fragment){
+        if(fragment != mFragmentNow){
+            mFragmentNow = fragment;
+            Bundle bundle = new Bundle();
+            bundle.putString("key","Activity Data");//Fragment数据传递
+            fragment.setArguments(bundle);
+            getFragmentManager().beginTransaction().replace(R.id.fl_content,fragment).commit();
+        }
     }
 }
