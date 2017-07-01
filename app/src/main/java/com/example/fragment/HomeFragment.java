b88deb0f00a6fee7439b57376e1208d373ab6e8b
phanz <phanz@qq.com>
2017-07-01 21:46:41 +0800

    滑动导航栏，临时提交3

--- app/src/main/java/com/example/fragment/HomeFragment.java
+++ app/src/main/java/com/example/fragment/HomeFragment.java
@@ -1,7 +1,5 @@
 package com.example.fragment;
 
-
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,14 +8,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.adapter.FragmentAdapter;
 import com.example.jokeeassy.R;
 import com.example.utils.DisplayUtils;
 import com.example.widgets.ColorTrackView;
+import com.example.widgets.NavigatorView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +22,8 @@ import java.util.List;
 public class HomeFragment extends Fragment implements ViewPager.OnPageChangeListener{
     public static final String TAG = "HomeFragment";
 
-    private static final int TAB_NUM = 6;
-    private int tabWidth;
-    private ImageView mCursorImage;
-    private LinearLayout.LayoutParams mCursorParams;
-
-    private LinearLayout mTabContainer;
+    private static final int TAB_NUM_DEFAULT = 6;
+    private NavigatorView mNavigatorView;
     private List<String> mTitleList;
     private List<Fragment> mHomeSubFragment;
 
@@ -71,33 +63,16 @@ public class HomeFragment extends Fragment implements ViewPager.OnPageChangeList
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         mViewPager = (ViewPager) view.findViewById(R.id.recommend_view_pager);
-        mCursorImage = (ImageView) view.findViewById(R.id.tab_cursor_image);
-        mCursorParams = (LinearLayout.LayoutParams) mCursorImage.getLayoutParams();
-        WindowManager windowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
-        int width = windowManager.getDefaultDisplay().getWidth();
-        tabWidth = width / TAB_NUM;
 
-        mTabContainer = (LinearLayout) view.findViewById(R.id.tab_list_layout2);
-
-        for(int i = 0; i < mTitleList.size(); i++){
-            final ColorTrackView tabView = makeColorTrackView(mTitleList.get(i));
-            if(i == 0) tabView.setProgress(1);
-            mTabContainer.addView(tabView);
-            ViewGroup.LayoutParams params = tabView.getLayoutParams();
-            params.width = tabWidth;
-            params.height = DisplayUtils.dp2px(getActivity(),40);
-            tabView.setLayoutParams(params);
-            final int index = i;
-            tabView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    /*ColorTrackView preTrackView = (ColorTrackView) mTabContainer.getChildAt(preIndex);
-                    if(preTrackView != null) preTrackView.setProgress(0);
-                    tabView.setProgress(1);*/
-                    mViewPager.setCurrentItem(index);
-                }
-            });
-        }
+        mNavigatorView = (NavigatorView) view.findViewById(R.id.tab_list_layout);
+        mNavigatorView.setTabCount(TAB_NUM_DEFAULT);
+        mNavigatorView.setTabList(mTitleList);
+        mNavigatorView.setOnTabListener(new NavigatorView.OnTabClickListener() {
+            @Override
+            public void onTabClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
 
         FragmentManager fm = getChildFragmentManager();
         mFragmentAdapter = new FragmentAdapter(fm,mHomeSubFragment);
@@ -109,18 +84,11 @@ public class HomeFragment extends Fragment implements ViewPager.OnPageChangeList
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        mCursorParams.leftMargin = positionOffsetPixels / TAB_NUM + tabWidth * position;
-        mCursorImage.setLayoutParams(mCursorParams);
-        String log = String.format("position:%d,\tpositionOffset:%f\tpositionOffsetPixels:%d",position,positionOffset,positionOffsetPixels);
+        String log = String.format("position:%d,\tpositionOffset:%f\tpositionOffsetPixels:%d",
+                position,positionOffset,positionOffsetPixels);
+
         Log.d(TAG,log);
-        if(positionOffset > 0){
-            ColorTrackView left = (ColorTrackView) mTabContainer.getChildAt(position);
-            ColorTrackView right = (ColorTrackView) mTabContainer.getChildAt(position + 1);
-            left.setDirection(1);
-            right.setDirection(0);
-            left.setProgress(1 - positionOffset);
-            right.setProgress(positionOffset);
-        }
+        mNavigatorView.setCursorPosition(position,positionOffset);
 
     }
 
--- app/src/main/java/com/example/widgets/NavigatorView.java
+++ app/src/main/java/com/example/widgets/NavigatorView.java
@@ -2,14 +2,18 @@ package com.example.widgets;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.example.jokeeassy.R;
+import com.example.utils.DisplayUtils;
 
 import java.util.List;
 
@@ -21,12 +25,21 @@ public class NavigatorView extends RelativeLayout {
 
     private Paint mPaint;
     private LinearLayout mTabLayout;
+
     private Context mContext;
 
-    private ImageView mCursorImage;
+    private static final int TAB_NUM_DEFAULT = 6;
+    private int mTabCount;
+    private int mScreenWidth;
+    private int tabWidth;
+    private int tabHeight;
 
+    private int mCursorOffsetX;
+    private int mCursorOffsetY;
     private int mCursorWidth;
     private int mCursorHeight;
+
+    public OnTabClickListener mOnTabClickListener;
     public NavigatorView(Context context) {
         super(context);
         init(context);
@@ -41,25 +54,61 @@ public class NavigatorView extends RelativeLayout {
         setWillNotDraw(false);//ViewGroup本身不含内容，默认为透明的，不触发onDraw操作，需要手动开启
         mContext = context;
         mPaint = new Paint();
-        mPaint.setColor(Color.YELLOW);
+        mPaint.setColor(context.getResources().getColor(R.color.tab_cursor_bg));
         LinearLayout.LayoutParams tabLayoutParams = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         mTabLayout = new LinearLayout(context);
         mTabLayout.setLayoutParams(tabLayoutParams);
         addView(mTabLayout);
 
-        mCursorHeight = 50;
-        mCursorWidth = 50;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        mScreenWidth = windowManager.getDefaultDisplay().getWidth();
+        setTabCount(TAB_NUM_DEFAULT);
+
+        mCursorOffsetX = 0;
+        mCursorOffsetY = 0;
+        mCursorWidth = tabWidth;
+        mCursorHeight = tabHeight;
     }
 
-    public void setTabList(List<TextView> tabList){
+    public void setTabList(List<String> tabList){
         if(tabList == null || tabList.size() == 0){
             return;
         }
 
-        for(TextView textView : tabList){
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(tabWidth,tabHeight);
+        for(int i = 0; i < tabList.size(); i++){
+            String title = tabList.get(i);
+            TextView textView = new TextView(mContext);
+            textView.setText(title);
+            textView.setGravity(Gravity.CENTER);
+            //textView.setBackgroundColor( (i % 2 == 1) ? Color.RED : Color.BLUE);
+            textView.setLayoutParams(params);
             mTabLayout.addView(textView);
+
+            textView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(mOnTabClickListener != null){
+                        int index = ((ViewGroup)view.getParent()).indexOfChild(view);
+                        mOnTabClickListener.onTabClick(index);
+                    }
+                }
+            });
+
         }
+    }
+
+    public void setTabCount(int tabCount){
+        mTabCount = tabCount;
+        tabWidth = mScreenWidth / mTabCount;
+        tabHeight = DisplayUtils.dp2px(mContext,40);
+        invalidate();
+    }
+
+    public void setCursorPosition(int position, float positionOffset){
+        mCursorOffsetX = position * tabWidth + (int)(positionOffset * tabWidth);
+        invalidate();
     }
 
     public void setCursorSize(int width,int height){
@@ -67,9 +116,21 @@ public class NavigatorView extends RelativeLayout {
         mCursorHeight = height;
     }
 
+    public void setOnTabListener(OnTabClickListener onTabListener){
+        mOnTabClickListener = onTabListener;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawRect(0,0,mCursorWidth,mCursorHeight,mPaint);
+        canvas.drawRect(mCursorOffsetX,
+                mCursorOffsetY,
+                mCursorOffsetX + mCursorWidth,
+                mCursorOffsetY + mCursorHeight,
+                mPaint);
+    }
+
+    public interface OnTabClickListener{
+        void onTabClick(int index);
     }
 }
